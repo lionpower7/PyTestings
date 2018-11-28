4e3f15fc94536c0fe15a2905d2808040f5fc41f0
Jeremy B. Frias <lionfocus07@gmail.com>
2018-11-27 22:12:33 -0500

    Pushing TO GIT

---
+++ Core.py
@@ -0,0 +1,15 @@
+import CoreMC
+
+#Z-Ten : Functions
+
+#Z-Ten Main : Program Logic
+def main():
+    start = int(input(" Press '1' to begin ... // "))
+
+    if start == 1:
+        print("Welcome To 'Z-Ten'")
+        measure = ['Capital Gains','Income']
+        timeframes = ['Daily','Weekly','Monthly','Yearly']
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,5 @@
+This mini-project is attempting to create a useful financial tool called the Z-Ten.
+
+Core.py - The Main File Of The Project.
+CoreMC.py - Will contain functions and classes that will be imported to Core.py.
+
