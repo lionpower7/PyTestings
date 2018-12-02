5bbd3e43b4794b2fa44750a5c9beae122322776b
Jeremy B. Frias <jeremyfd77@gmail.com>
2018-12-02 00:03:41 -0500

    Changing Project Goal Once Again

--- Core.py
+++
@@ -1,8 +0,0 @@
-# Fibonacci - is a list of numbers which every number is a result of itself added to its previous number.
-# 0,1,1,2,3,5
-# 0 + 1 Makes 1, 1 + 1 Makes 2, 2 + 1 Makes 3 & etc...
-
-from CoreMC import fibonate
-
-amount = int(input("Enter The Amount Of Fibonacci Numbers You Wish To See: "))
-fibonate(amount)
